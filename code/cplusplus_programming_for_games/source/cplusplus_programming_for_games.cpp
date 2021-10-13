576811e5a62f528fc47654bf34fe01c6e9fc2eea
Gian-Dev2020 <72135815+Gian-Dev2020@users.noreply.github.com>
2021-10-13 21:06:49 +0100

    SDL2 Linked

--- code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
+++ code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
@@ -1,6 +1,7 @@
 // cplusplus_programming_for_games.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+#define SDL_MAIN_HANDLED
 #include <iostream>
+#include <SDL.h>
 
 using namespace std;
 int main()
